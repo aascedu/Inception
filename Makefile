4dd85e1d63557037e9fdfdce132161b9beb124cd
aascedu <arthurascedusnkrs@gmail.com>
2024-02-14 08:13:46 -0600

    Jai affiche quelque chose tack sa mycket thea WANG

---
+++ Makefile
@@ -0,0 +1,28 @@
+include srcs/.env
+
+COMPOSE	= docker compose -f
+COMPOSE_SRC = srcs/docker-compose.yml
+
+all: volumes
+	$(COMPOSE) $(COMPOSE_SRC) up -d --build
+
+up: volumes
+	$(COMPOSE) $(COMPOSE_SRC) up -d
+
+down:
+	$(COMPOSE) $(COMPOSE_SRC) down
+
+prune:
+	$(COMPOSE) $(COMPOSE_SRC) stop
+	docker system prune -af;
+	docker volume prune -af;
+
+volumes:
+	mkdir -p /home/aascedu/data/mariadb
+	mkdir -p /home/aascedu/data/wordpress
+
+clean:
+	$(COMPOSE) $(COMPOSE_SRC) down --volumes --rmi all
+	rm -rf /home/aascedu/data/mariadb /home/aascedu/data/mariadb
+
+.PHONY: all volumes clean up down
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,18 +1,29 @@
 #!bin/bash
-sleep 10
 
-if [ -d "/run/php" ]; then
-    :
-else
+if [ ! -d "/run/php" ]; then
     mkdir -p /run/php
 fi
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+
+wp core download --path=/var/www/html \
+                 --allow-root
 
 wp config create	--allow-root \
                     --dbname=$SQL_DATABASE \
                     --dbuser=$SQL_USER \
                     --dbpass=$SQL_PASSWORD \
-                    --dbhost=mariadb:3306 --path='/var/www/wordpress'
+                    --dbhost=mariadb:3306 \
+                    --path=/var/www/html
 
-wp core install
+# CHANGER URL
+wp core install     --allow-root \
+                    --url=aascedu.42.fr \
+                    --admin_user=$SQL_USER \
+                    --admin_password=$SQL_PASSWORD \
+                    --admin_email=whwh@wh.com \
+                    --title=Inception \
+                    --path=/var/www/html
+fi
 
 /usr/sbin/php-fpm7.4 -F
